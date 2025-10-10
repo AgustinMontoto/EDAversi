9551575c4ed5d51a86b52f0b89f50b1924b8fcf2
sol <mvigilante@itba.edu.ar>
2025-10-09 23:15:43 -0300

    .. git commit -m ..

--- ai.cpp
+++ ai.cpp
@@ -82,15 +82,16 @@ void Recorrido_BFS(Tree_level_t& Tree_level){     //uso el algoritmo BFS para la
 
 // Algoritmo Minimax recursivo
 int minimax(treeNode* node, Player ia_player, int depth, int maxDepth, int &g_nodesExplored) {
+    static int maxValue = -1000;       //valor random muy chico para el caso del primer hijo   
+    static int minValue = 1000;
     // Caso base: nodo hoja
     g_nodesExplored ++;
-    if (depth>=maxDepth || node->children.empty()) {
+    if (maxValue >= minValue || node->children.empty()) {
         return value_state(node->model);
     }
     
     //evaluo caso inductivo
-    if (node->model.currentPlayer == ia_player) {     // Turno de la IA: maximizar
-        int maxValue = -1000;       //valor random muy chico para el caso del primer hijo       
+    if (node->model.currentPlayer == ia_player) {     // Turno de la IA: maximizar    
         
         for (auto child : node->children) {     //se fija en todos sus nodos hijos a ver cual tiene el mayor valor y lo guarda
             int value = minimax(child, ia_player, depth + 1, maxDepth, g_nodesExplored);
@@ -101,7 +102,6 @@ int minimax(treeNode* node, Player ia_player, int depth, int maxDepth, int &g_no
         return maxValue;
         
     } else {                // Turno del humano: minimizar
-        int minValue = 1000;
         
         for (auto child : node->children) {
             int value = minimax(child, ia_player, depth + 1, maxDepth, g_nodesExplored);
